6a05c9621461c80d98269ba56e8bac4fea9a695d
id2590-ensai <ousseynou.simal@eleve.ensai.fr>
2025-09-12 10:47:59 +0000

    ajout new

--- src/client/attack_client.py
+++ src/client/attack_client.py
@@ -36,14 +36,66 @@ class AttackClient(metaclass=Singleton):
         # Check if the request is ok
         if req.status_code == 200:
             raw_attack = req.json()
+            # Les element qui sont renvoyés ont cette forme
+            # {
+            #    "name": "<An awesome name>",
+            #    "attack_type": "<An awesome type>",
+            #    "power": 0,
+            #    "accuracy": 0,
+            #    "element": "<An awesome element>",
+            #    "description": "<An awesome description>"
+            # }
 
             print("Réponse JSON obtenue :\n" + json.dumps(raw_attack, indent=2) + "\n")
 
-            # TODO
             #   create an attack using the data contained in the json
             #   see class AttackFactory to do this
+            attack = AttackFactory().instantiate_attack(
+                type=raw_attack["attack_type"],
+                id=raw_attack["id"],
+                power=raw_attack["power"],
+                name=raw_attack["name"],
+                description=raw_attack["description"],
+                accuracy=raw_attack["accuracy"],
+                element=raw_attack["element"],
+            )
 
         return attack
+
+    def get_all_attacks(self) -> list(AbstractAttack):
+        """
+            Cette méthode renvoie la liste de toutes les attaques disponibles sous 
+            la forme d'une liste d'objets AbstractAttack.
+        """
+
+        url = f"{self.__HOST}{END_POINT}/"
+        print("GET  " + url + "\n")
+        req = requests.get(url)
+
+        results = []
+
+        # Check if the request is ok
+        if req.status_code == 200:
+            raw_attack = req.json()
+
+            # Création d'une liste d'objet de type
+            # Récupé de chaque json
+            for elm in raw_attack["results"]:
+            
+                #   create an attack using the data contained in the json
+                #   see class AttackFactory to do this
+                attack = AttackFactory().instantiate_attack(
+                    type=elm["attack_type"],
+                    id=elm["id"],
+                    power=elm["power"],
+                    name=elm["name"],
+                    description=elm["description"],
+                    accuracy=elm["accuracy"],
+                    element=elm["element"],
+                )
+                results.append(attack)
+
+        return results
 
 
 # Execute Code When the File Runs as a Script
